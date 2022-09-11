4a2faa831b129238f73bc07c1d74e2250bf409d4
skril <SZhukovWork@gmail.com>
2022-09-12 01:37:11 +0500

    animations page-player

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,4 +1,15 @@
 
 document.querySelectorAll('.page-player').forEach(item=>{
-    item.addEventListener('')
+    let onWait = false;
+    item.addEventListener('mouseover',e=>{
+        onWait = false;
+    })
+    item.addEventListener('mouseenter',e=>{
+        onWait = true
+        setTimeout(()=>{
+            if(onWait){
+                e.target.classList.add('active');
+            }
+        },3000)
+    })
 })
